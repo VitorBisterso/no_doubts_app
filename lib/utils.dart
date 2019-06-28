5986caaf1b5b7d6d16b47f252d52e7b49ef7432d
Vitor Bisterso dos Santos <vitor.santos@dextra-sw.com>
2019-06-28 09:26:41 -0300

    Remove unused validate package

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,3 @@
-import 'package:validate/validate.dart';
-
 String validateEmail(String email) {
   RegExp regExp = new RegExp(
     r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$",
